686b069f7e2b15c68624221d112de928162d136b
Joe Courtney <jcourtney@edsal.com>
2018-01-08 23:47:17 -0600

    Setup separate test and development environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ var {User} = require('./models/user.js');
 
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -107,7 +109,7 @@ app.patch('/todos/:id', (req, res) => {
         }
 
         res.status(200).send({todo});
-        
+
     }).catch((e) => {
         res.status(400).send();
     });
